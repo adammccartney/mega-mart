7cefbf79cc9d2e274062e7c95cde971a5f963301
Adam McCartney <mccartney@mdw.ac.at>
2022-07-19 22:56:28 +0200

    Show roots of revela module pattern
    This is how to roll your own namespace in js

--- src/index.js
+++ src/index.js
@@ -1,58 +1,58 @@
+const megaMart = (() => {
+    const items = [
+          {name: "Shirt", price: "€15"},
+          {name: "Jeans", price: "€12"}
+    ]
 
-class Item {
-  constructor(name, price) {
-    this.name = name
-    this.price = price
+    const create_item_description = (item) => {
+        // creates a dom node with item's description
+        let description = document.createElement("div")
+        description.classList.add("item-description")
+        description.appendChild(elt("strong", `${item.name}`))
+      }
+
+  const exported = {
+    items: items,
+    create_item_description: create_item_description
   }
-}
-
-const items = [
-    {name: "Shirt", price: "€15"},
-    {name: "Jeans", price: "€12"}
-]
-
-function elt(type, ...children) {
-  // 
-  let node = document.createElement(type);
-  for (let child of children) {
-    if (typeof child != "item") node.appendChild(child);
-    else node.appendChild(document.createElement(child));
-  }
-  return node;
-}
-
-function create_item_description(item) {
-  // creates a dom node with item's description
-  let description = document.createElement("div")
-  description.classList.add("item-description")
-  description.appendChild(elt("strong", `${item.name}`))
-}
-
-function element_from_item (item) {
-    // this is an action
-    let node = document.body.getElementByTagName("li")
-    let div = document.createElement('div')
-    div.classList.add('item')
-    div.appendChild(p)
-    return div
-}
-
-function render_items_dom (items) {
-    for (let i = 0; i < items.length(); i++) {
-        let div = 
-        node.appendChild(document.createElement("div class=item"))
-    }
+  return exported;
+})()
   
-}
+  
 
-var shopping_cart = [];
-var shopping_cart_total = [];
-
-
-
-function add_item_to_cart(name, price) {
-  shopping_cart.push({
-    name: name,
-    price: price
-  })
-}
+//function elt(type, ...children) {
+//  // 
+//  let node = document.createElement(type);
+//  for (let child of children) {
+//    if (typeof child != "item") node.appendChild(child);
+//    else node.appendChild(document.createElement(child));
+//  }
+//  return node;
+//}
+//
+//function create_item_elt (item) {
+//    // this is an action
+//    // it creates an Element of the type item
+//    let node = document.body.getElementByTagName("li")
+//    let div = document.createElement('div')
+//    div.classList.add('item')
+//    Div.appendChild(p)
+//    return div
+//}
+//
+//function render_items_dom (items) {
+//    for (let i = 0; i < items.length(); i++) {
+//        let div = 
+//        node.appendChild(document.createElement("div class=item"))
+//    }
+//}
+//
+//var shopping_cart = [];
+//var shopping_cart_total = [];
+//
+//function add_item_to_cart(name, price) {
+//  shopping_cart.push({
+//    name: name,
+//    price: price
+//  })
+//}
