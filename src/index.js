54cf1f703924ab0ce5cabf68131a4695e5ad0849
Adam McCartney <mccartney@mdw.ac.at>
2022-07-25 19:52:31 +0200

    Include changes to npm boilerplate stuff

--- src/index.js
+++ src/index.js
@@ -1,37 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-
-import layoutmodule from './components/layout.jsx'
-const Layout = layoutmodule.Layout;
-
-//const App = () => (
-//  <Navbar />
-//  //<div
-//  //  style={{
-//  //    display: 'flex',
-//  //    alignItems: 'center',
-//  //    justifyContent: 'center',
-//  //    height: '100vh',
-//  //  }}
-//  //>
-//
-//  //  <h1>
-//  //    Hello World from <a href="https://www.polynique.com">Polynique</a>
-//  //  </h1>
-//  //</div>
-//)
-//
-//ReactDOM.render(<App />, document.getElementById('app'))
-
+import App from './App';
+import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
 );
 
-const navprops = {
-  title: "Mega Mart"
-}
-
-const element = <Layout home/>;
-root.render(element);
+root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+);
