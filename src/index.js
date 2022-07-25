00db8d14123ec8435347f2930abc5cb5ff58811d
Adam McCartney <mccartney@mdw.ac.at>
2022-07-25 08:23:45 +0200

    First component
    Changed over to react and wrote the first component (navbar).
    Otherwise just set up some boilerplate and structured the project.
    Also added some notes to org-roam.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
-import megaMart from "./mega-mart.js"
-
-let items = megaMart.items;
-let nav_data = megaMart.nav_data;
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 
-// render our components
-megaMart.make_nav_display_dom(nav_data);
-megaMart.make_item_display_dom(items);
+import layoutmodule from './components/layout.jsx'
+const Layout = layoutmodule.Layout;
+
+//const App = () => (
+//  <Navbar />
+//  //<div
+//  //  style={{
+//  //    display: 'flex',
+//  //    alignItems: 'center',
+//  //    justifyContent: 'center',
+//  //    height: '100vh',
+//  //  }}
+//  //>
+//
+//  //  <h1>
+//  //    Hello World from <a href="https://www.polynique.com">Polynique</a>
+//  //  </h1>
+//  //</div>
+//)
+//
+//ReactDOM.render(<App />, document.getElementById('app'))
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+
+const navprops = {
+  title: "Mega Mart"
+}
+
+const element = <Layout home/>;
+root.render(element);
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,23 +1,34 @@
 const path = require("path");
+// add webpack import
+const webpack = require('webpack') 
 
 module.exports = {
   entry: './src/index.js',
-    mode: 'development',
-  devServer: {
-
-  static: './dist',
-
-   },
-   output: {
+  output: {
      filename: 'bundle.js',
      path: path.resolve(__dirname, 'dist'),
+     publicPath: '/dist/',
    },
    module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
         use: ['style-loader', 'css-loader'],
       },
     ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    hot: true,
   },
 };
